7cb5d40accc201101480a6872af3223b730d4fc7
manu <cascio713@gmail.com>
2022-07-27 17:13:40 +0200

    started vectorization of a sentence

--- Document.py
+++ Document.py
@@ -1,7 +1,74 @@
 class Document:
 
-    def __init__(self, nlp, text):
-
+    def __init__(self, nlp, text, name, title):
+        self.name = name
+        self.title = title
+        self.doc = nlp(text)
         self.text = text
         self.TFIDF = dict()
-        self.terms = self.TFIDF.keys()
+        self.terms = set([token.text for token in self.doc])
+        self.keywords = set()
+
+    def compute_tfidf(self, idf):
+        for term in self.terms:
+            count = 0
+            for token in self.doc:
+                count += int(term == token.text)
+            self.TFIDF[term] = (count / len(self.doc)) * idf[term]
+        keywords = {k: v for k, v in sorted(self.TFIDF.items(), key=lambda item: item[1], reverse=True)}
+        counter = 0
+        for kw in keywords:
+            self.keywords |= {kw}
+            if counter == 5:
+                break
+            counter = counter + 1
+
+    def contains(self, term: str):
+        return term in self.terms
+
+    def print_keywords(self):
+        keywords = {k: v for k, v in sorted(self.TFIDF.items(), key=lambda item: item[1], reverse=True)}
+        counter = 0
+        for kw in keywords:
+            print(kw, keywords[kw])
+            if counter == 5:
+                break
+            counter = counter + 1
+
+    def title_resemblance(self, sentence):
+        intersection = 0
+        union = 0
+        for kw in self.keywords:
+            if kw in self.title and kw in sentence.text:
+                intersection += 1
+            if kw in self.title or kw in sentence.text:
+                union += 1
+        # cover the case of a title with no keyword
+        union = max(1, union)
+        return intersection / union
+
+    def coverage(self, sentence):
+        intersection = 0
+        for kw in self.keywords:
+            if kw in sentence.text:
+                intersection += 1
+        return intersection / len(self.keywords)
+
+    @staticmethod
+    def proper_entities(sentence):
+        return len(sentence.ents) / len(sentence)
+
+    @staticmethod
+    def numerical_words(sentence):
+        numbers = 0
+        for token in sentence:
+            try:
+                float(token.text)
+                numbers += 1
+            except ValueError:
+                pass
+        return numbers / len(sentence)
+    
+    def relative_length(self, sentence):
+        # return (len(sentence) * len(self.doc.sents)) / len(self.doc) i don't get whu the constant multiplier
+        return len(sentence) / len(self.doc)
--- Summarizer.py
+++ Summarizer.py
@@ -1,16 +1,50 @@
 from math import log10
+from Document import Document
+from os import listdir, sep
+import fitz
 
 
 class Summarizer:
 
-    def __init__(self):
+    @staticmethod
+    def _get_text(path: str) -> (str, str):
+        text = ""
+        title = ""
+        extension = path.split('.')[-1]
+        if extension == "txt":
+            document = open(path, "r")
+            # in text files the title is assumed to be the file name
+            title = path.split('/')[-1].split('.')[-1].replace("_", " ")
+            for line in document:
+                text += line
+        elif extension == "pdf":
+            document = fitz.open(path)
+            title = document.title
+            for page in document:
+                text += page.get_text()
+        else:
+            raise Exception("Cannot determine the file type")
+        return text
+
+    def __init__(self, nlp, document_folder):
         self.IDF = dict()  # term -> term idf score
         self.terms = self.IDF.keys()
-        self.documents = list()
+        self.nlp = nlp
+        self.documents: list[Document] = list()
+        for file in listdir(document_folder):
+            text = ""
+            title = ""
+            try:
+                text, title = self._get_text(document_folder + sep + file)
+            except Exception as error:
+                print(file, error)
+            self.documents.append(Document(nlp, text, file, title))
+        self.get_terms()
+        self.compute_scores()
 
-    def get_keywords(self):
+    def get_terms(self):
         for doc in self.documents:
-            self.terms |= self.documents.terms
+            self.terms |= doc.terms
 
     def _get_term_frequency(self, term: str):
         frequency = 0
@@ -24,4 +58,11 @@ class Summarizer:
             self.IDF[term] = log10(len(self.documents) / self._get_term_frequency(term))
         # step 2: computer tf-idf
         for doc in self.documents:
-            doc.compute_TF_IDF(self.IDF)
+            doc.compute_tfidf(self.IDF)
+
+    def print(self):
+        for i in range(len(self.documents)):
+            print("\n\nDocument\t", self.documents[i].name)
+            print("Term\tTerm tfidf score")
+            self.documents[i].print_keywords()
+
